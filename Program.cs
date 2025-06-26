17839e25ce911c5664d0cd2da77141d4cc4befa9
Diyaa Ebrahim <74496604+diyaaebrahimm@users.noreply.github.com>
2025-06-26 02:33:44 -0700

    Enable Swagger and add test endpoint

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using SecureFileSharingAPI;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("Default") ?? "Data Source=app.db"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<ContextAwareRateLimiterMiddleware>();
 
 using (var scope = app.Services.CreateScope())
 {
@@ -15,5 +25,6 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.MapGet("/", () => "Secure File Sharing API");
+app.MapGet("/api/test/ping", () => Results.Ok());
 
 app.Run();
